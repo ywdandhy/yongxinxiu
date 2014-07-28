75baaccd1a051ea2813acddbcfa7f3adfe04d879
ywd <ywdandhy@163.com>
2014-07-28 09:55:42 +0800

    工程建立

---
+++ yongxinxiu/src/main/java/controller/IndexPageController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexPageController {
+
+	@RequestMapping("index")
+	private Object indexPage() {
+		ModelAndView modelAndView = new ModelAndView("/index.ftl");
+		modelAndView.addObject("hellow", "helloWord你妹");
+		return modelAndView;
+	}
+}
---
+++ yongxinxiu/src/main/webapp/index.ftl
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>${hellow}</h2>
+</body>
+</html>
