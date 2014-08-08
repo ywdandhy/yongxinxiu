3a08af4e52842cef65e46546396f6c07742fa482
ywd <ywdandhy@163.com>
2014-08-08 16:41:02 +0800

    集成前台框架kendo

--- yongxinxiu/src/main/webapp/WEB-INF/view/index.ftl
+++ yongxinxiu/src/main/webapp/WEB-INF/view/index.ftl
@@ -1,5 +1,6 @@
 <html>
 <body>
+<#include "/view/resource/resource.ftl" />
 <h2>${hellow}</h2>
 </body>
 </html>
--- yongxinxiu/src/test/java/com/ywd/Dao/TestUser.java
+++ yongxinxiu/src/test/java/com/ywd/Dao/TestUser.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.ywd.model.User;
 import com.ywd.service.User.IUserService;
